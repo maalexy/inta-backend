5e738d18ed2a6d2ef9cc6c335932b6a9c7051ab1
Alexy Marcell <alexy.marcell@hotmail.com>
2020-04-06 23:08:43 +0200

    Changed login interfaces

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import bcrypt
 ### Inits, setups
 app = Flask(__name__)
 
-app.config['JWT_SECRET_KEY'] = os.environ.get('CICAPP_BACKEND_JWT_SECRET',os.getrandom(16))
+app.config['JWT_SECRET_KEY'] = os.environ.get('INAT_BACKEND_JWT_SECRET',os.getrandom(16))
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(hours=4)
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
 
-app.config['SECRET_KEY'] = os.environ.get('CICAPP_BACKEND_SECRET', '123456')
+app.config['SECRET_KEY'] = os.environ.get('INTA_BACKEND_SECRET', '123456')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///.local/lite.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -33,41 +33,59 @@ def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return jti in blacklist_jwt
 
+@jwt.expired_token_loader
+@jwt.invalid_token_loader
+@jwt.unauthorized_loader
+def token_error()
+    return 
+
 ### Endpoints
 
 @app.route('/helloworld')
 def hello_world():
     return "HelloWorld"
 
-# Parameters: username, password, email
-# Parameters will be given through POST body form
 @app.route('/register', methods=['POST'])
 def register():
-    usern = request.form['username']
-    passw = request.form['password']
-    pwhash = bcrypt.hashpw(passw.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    email_ = request.form['email']
+    data = request.get_json()
+    user = data['user']
+    password = data['password']
+    pwhash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    email = data['email']
     if User.query.filter_by(username=usern).first() != None:
-        return jsonify(msg='User already registered'), 403
-    new_user = User(username=usern, password=pwhash, email=email_)
+        return jsonify(error='Username is already in use'), 409
+    new_user = User(username=usern, password=pwhash, email=email_f 
     db.session.add(new_user)
     db.session.commit()
     return jsonify(msg='Success'), 200
 
-
-# Parameters: username, password
-# Parameters will be given through POST body form
 @app.route('/login', methods=['POST'])
 def login():
-    usern = request.form['username']
-    passw = request.form['password']
+    data = request.get_json()
+    username = request.form['username']
+    password = request.form['password']
     user = User.query.filter_by(username=usern).first()
     if user == None:
-        return jsonify(msg='User not registered'), 401
-    if not bcrypt.checkpw(passw.encode('utf-8'), user.password.encode('utf-8')):
-        return jsonify(msg='Bad creditentials'), 401
+        return jsonify(error='Wrong username or password'), 401
+    if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+        return jsonify(error='Wrong username or password'), 401
     token = create_access_token(identity=usern)
     return jsonify(msg='Success', token=token), 200
+
+@app.route('/passwordreset')
+def passwordreset():
+    data = request.get_json()
+    username = request['user']
+    email = request['email']
+    user = None
+    if username:
+        user = User.query.filter_by(username=username).first()
+    if email:
+        user = User.query.filter_by(email=email).first()
+    if user == None:
+        return jsonify(error='User not found'), 404
+    # send email to user.email
+    return jsonify(msg='Success'), 200
 
 
 @app.route('/.dev/jwtecho')
